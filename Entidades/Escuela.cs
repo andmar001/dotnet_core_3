3420792b088fc6bfd63c559a9be88ecee70514d4
tony tolver <andrademaresantonio00@gmail.com>
2023-01-02 00:09:15 -0600

    implementacion de las interfaces

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
+using CoreEscuela.Util;
 
 namespace CoreEscuela.Entidades
 {
-    public class Escuela:ObjetoEscuelaBase
+    public class Escuela:ObjetoEscuelaBase, ILugar
     {
         public int AñoDeCreación { get; set; }
 
         public string Pais { get; set; }
         public string Ciudad { get; set; }
+        public string Direccion  { get; set; }               
         public TiposEscuela TipoEscuela { get; set; }
         public List<Curso> Cursos { get; set; }
 
@@ -27,5 +29,19 @@ namespace CoreEscuela.Entidades
         {
             return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad:{Ciudad}";
         }
+
+        public void LimpiarLugar()
+        {
+            Printer.DrawLine();
+            Console.WriteLine("Limpiando Escuela...");
+            
+            foreach (var curso in Cursos)
+            {
+                curso.LimpiarLugar();
+            }
+            
+            Console.WriteLine($"Escuela {Nombre} Limpio...");
+        }
+        
     }
 }
--- Entidades/ObjetoEscuelaBase.cs
+++ Entidades/ObjetoEscuelaBase.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace CoreEscuela.Entidades
 {
-    public class ObjetoEscuelaBase
+    public abstract class ObjetoEscuelaBase
     {
         public string UniqueId { get; private set; }
         public string Nombre { get; set; }
